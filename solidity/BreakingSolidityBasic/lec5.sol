3512bd04b37742a992993e10ce5a9ed9cb94add6
JuneKei <85489867+JuneKei@users.noreply.github.com>
2022-05-10 20:03:26 +0900

    Create lec5.sol

---
+++ solidity/BreakingSolidityBasic/lec5.sol
@@ -0,0 +1,11 @@
+//SPDX-license-Identifier : GPL-30
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract lec5{
+    /*
+        public : 모든곳에서 접근 가능
+        external : public처럼 모든곳에서 접근 가능하나, external이 정의된 자기자신 contract 내에서 접근 불가
+        private : 오직 private이 정의된 자기자신 contract에서만 가능(private이 정의된 contract를 상속 받은 자식도 불가능)
+        internal : private 처럼 오직 interal 이정의된 자기 contract에서만 가능하고, internal이 정의된 contract도 접근 가능
+     */
+}
